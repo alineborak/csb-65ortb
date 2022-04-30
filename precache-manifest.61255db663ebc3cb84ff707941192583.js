5014592e102489b94c82234e14aebac33162d109
Code Sandbox <hello@codesandbox.io>
2022-04-30 13:48:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.61255db663ebc3cb84ff707941192583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de8a237554859314cc9",
+    "url": "https://alineborak.github.io/csb-65ortb/static/css/main.b0d7e3a0.chunk.css"
+  },
+  {
+    "revision": "4de8a237554859314cc9",
+    "url": "https://alineborak.github.io/csb-65ortb/static/js/main.9a8410fd.chunk.js"
+  },
+  {
+    "revision": "35ae8e862ff99464161e",
+    "url": "https://alineborak.github.io/csb-65ortb/static/js/runtime~main.cf4e5bd2.js"
+  },
+  {
+    "revision": "0c16f70c9f23915a7ab9",
+    "url": "https://alineborak.github.io/csb-65ortb/static/js/2.b35679a5.chunk.js"
+  },
+  {
+    "revision": "bf743478aec0ec3fd0dfa7cc822d2a91",
+    "url": "https://alineborak.github.io/csb-65ortb/index.html"
+  }
+];
